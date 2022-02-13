02273311b0219084a29f73bc109f8f551c3025af
kosuke-17 <tamutamu17@icloud.com>
2022-02-13 10:57:57 +0900

    記事取得クエリ作成

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -1,7 +1,12 @@
+import { Post } from "./models/Post.model";
+
 export const resolvers = {
   Query: {
     hello: () => {
       return "Hello World";
+    },
+    getAll: async () => {
+      return await Post.find();
     },
   },
 };
--- src/typeDefs.ts
+++ src/typeDefs.ts
@@ -1,7 +1,13 @@
 import { gql } from "apollo-server-express";
 
 export const typeDefs = gql`
+  type Post {
+    id: ID
+    title: String
+    description: String
+  }
   type Query {
     hello: String
+    getAll: [Post]
   }
 `;
